1c6f124068e45831d9ef3fddf8f4590f0ef8316d
kamrul1717 <kamrulhassan1361@gmail.com>
2018-08-31 21:22:57 -0700

    initial commit for my speed Timer App using javascript

---
+++ js/main.js
@@ -0,0 +1,122 @@
+window.addEventListener('load', init);
+
+// Globals
+
+// Available levels
+const levels = {
+	easy: 5,
+	medium: 3,
+	hard: 2
+}
+
+// To Change level
+const currentLevel = levels.medium;
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+// DOM elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+const words = [
+	'lucky',
+	'father',
+	'mother',
+	'sister',
+	'grandfather',
+	'children',
+	'zoology',
+	'Biology',
+	'hat',
+	'river',
+	'Afghanistan',
+	'Albania',
+	'Algeria',
+	'Andorra',
+	'Angola',
+	'Antigua and Barbuda',
+	'Argentina'
+];
+
+// Initialize game
+function init(){
+	// Show number of seconds in UI
+	seconds.innerHTML = currentLevel;
+
+	// Load word from array
+	showWord(words);
+
+	// Start matching on word input
+	wordInput.addEventListener('input', startMatch)
+
+	// Call countdown every second
+	setInterval(countdown, 1000);
+
+	// Check game status
+	setInterval(checkStatus, 50)
+}
+
+// Pick and show random word
+function showWord(words) {
+	// Generate random array index
+	const randIndex = Math.floor(Math.random() * words.length);
+
+	// Output random word
+	currentWord.innerHTML = words[randIndex];
+}
+
+// Start Match
+function startMatch(){
+	if(matchWords()){
+		isPlaying = true;
+		time = currentLevel+1;
+		showWord(words);
+		wordInput.value = '';
+		score++;
+	}
+	// If score is -1 display 0 
+	if(score === -1){
+		scoreDisplay.innerHTML = 0;
+	}else{
+		scoreDisplay.innerHTML = score;
+	}
+}
+
+// Match currentWord to wordInput
+function matchWords(){
+	if(wordInput.value === currentWord.innerHTML){
+			message.innerHTML = 'Correct!!!';
+			return true;
+		}else{
+			message.innerHTML = '';
+			return false;
+		}
+}
+
+// Countdown Timer
+function countdown(){
+	// Make sure time is not run out
+	if(time > 0){
+		// Decrement 
+		time--;
+	}else if(time === 0){
+		// Game is over
+		isPlaying = false;
+	}
+
+	// Show time
+	timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus(){
+	if(!isPlaying && time === 0){
+		message.innerHTML = 'Game Over!!!';
+		score = -1;
+	}
+}
